5b1272d42b6acd0f0afb1ff75609179dcefabe29
thewhiteninja <40250307+thewhiteninja@users.noreply.github.com>
2020-11-04 11:33:10 +0100

    Add semicolon at the end of qrcode data

--- qr.py
+++ qr.py
@@ -16,14 +16,14 @@ def generateQR(profile, trip):
 
     created_date = trip.date - timedelta(minutes=10)
 
-    textdata = ";\n ".join([
+    textdata = ";\n".join([
         "Cree le: %s a %s" % (created_date.strftime("%d/%m/%Y"), created_date.strftime("%H:%M")),
         "Nom: %s" % profile.lastname,
         "Prenom: %s" % profile.firstname,
         "Naissance: %s a %s" % (profile.birthday, profile.placeofbirth),
         "Adresse: %s %s %s" % (profile.address, profile.zipcode, profile.city),
         "Sortie: %s a %s" % (trip.date.strftime("%d/%m/%Y"), trip.date.strftime("%H:%M")),
-        "Motifs: %s" % ", ".join([str(x)[11:] for x in trip.reasons])
+        "Motifs: %s;" % ", ".join([str(x)[11:] for x in trip.reasons])
     ])
 
     q.add_data(textdata)
